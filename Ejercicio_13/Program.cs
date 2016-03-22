e3ce0936b781d0effb60a40b3cb14ada22c4fbc1
RomeroFederico <romerof14@gmail.com>
2016-03-22 17:31:30 -0300

    Main y clase Conversor creadas.

---
+++ Ejercicio_13/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio_13
+{
+    /*    
+    13.  Desarrollar una clase llamada 'Conversor', que posea dos métodos 
+    de clase (estáticos): 
+    string DecimalBinario(double). Convierte un número de 
+    decimal a binario. 
+    double BinarioDecimal(string). Convierte un número binario a 
+    decimal.  
+    */
+
+    class Ejercicio_13
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro. 13";
+        }
+    }
+}
